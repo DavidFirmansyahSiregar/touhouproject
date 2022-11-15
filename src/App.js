a8fa6ec55ff127a38da819d1399cfd2a6c429871
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-11-15 13:32:17 +0700

    battlefield

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <div className='column'></div>
+    
+    <div>
+      <div>
+        <div className='column1'></div>
+        <div className='column2'></div>
+      </div>
+      <div>
+        <div className='column3'></div>
+        <div className='column4'></div>
+      </div>
+      <div className='content'>
+        <p>attack</p>
+        <p>defend</p>
+        <p>item</p>
+        <p>magic</p>
+        <p>retreat</p>
+      </div>
     </div>
-  );
+    
+  )
 }
 
 export default App;
