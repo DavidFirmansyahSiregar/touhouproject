cbcbbea6dcd2de85292da22ef24b2da2cc13805f
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-11-29 16:42:28 +0700

    masihsibuk

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 export const login = () => {
+  var a;
+  let i = 1;
+  a = i++;
+  console.println("a = " + a);
+  console.println("i = " + i);
   return (
     <div>login</div>
   )
--- src/auth/signup.js
+++ src/auth/signup.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 export const signup = () => {
+  let gelas = 0;
+  gelas = 5;
+  gelas++;
+  console.println(gelas);
+
+  gelas = 5;
+  gelas = gelas + 1;
   return (
     <div>signup</div>
   )
