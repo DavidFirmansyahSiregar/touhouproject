73d43d5cb80371a0acb400d09577e34eb03a8b48
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-05 22:50:21 +0700

    home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './home.css';
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <h2>CSS Newsletter</h2>
+
+    <form action="/action_page.php">
+      <div className="container">
+        <h2>Subscribe to our Newsletter</h2>
+        <p></p>
+      </div>
+      <div className="container">
+        <input type="submit" value="Subscribe"></input>
+      </div>
+     
+    </form>
+    </div>
   )
 }
