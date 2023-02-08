cb2e9e91414c693f5c537c662978179ed41449e3
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2023-02-08 14:12:30 +0700

    games

--- src/Pages/Games/helper/shuffle.js
+++ src/Pages/Games/helper/shuffle.js
@@ -1,13 +1,19 @@
-import EmberSVG from "../img/ember.svg";
-import NodeSVG from "../img/node.svg";
-import ReactSVG from "../img/react.svg";
-import VueSVG from "../img/vue.svg";
-import SvelteSVG from "../img/svelte.svg";
-import AngularSVG from "../img/angular.svg";
+// import EmberSVG from "../img/ember.svg";
+// import NodeSVG from "../img/node.svg";
+// import ReactSVG from "../img/react.svg";
+// import VueSVG from "../img/vue.svg";
+// import SvelteSVG from "../img/svelte.svg";
+// import AngularSVG from "../img/angular.svg";
+import Reimu from "../img/reimu1.jpg";
+import Sakuya from "../img/sakuya1.jpg";
+import Komano from "../img/komano1.png";
+import Aya from "../img/aya1.png";
+import Alice from "../img/alice1.png";
+import Verra from "../img/verra.jpeg"
 
 export const createMatrix = () => {
-  const images = [EmberSVG, NodeSVG, SvelteSVG, AngularSVG, ReactSVG, VueSVG];  
-  const distinct = ["ember", "node", "svelte", "angular", "react", "vue"];
+  const images = [Reimu, Sakuya, Komano, Aya, Alice, Verra];  
+  const distinct = ["Reimu", "Sakuya", "Komano", "Aya", "Alice", "Verra"];
   const matrix = [];
   for (let i = 0; i < distinct.length; i++) {
     const cell = {
