0566785d93b481513d9575faaf24e91ba93f545c
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2023-03-14 23:08:14 +0700

    setupchar

---
+++ src/shared/characters.js
@@ -0,0 +1,24 @@
+export const playerStats = {
+
+  level: 1,
+  maxHealth: 50,
+  name: 'Sakuya',
+  // img: '/assets/samus.png',
+
+  magic: 5,
+  attack: 5,
+  defense: 5,
+  magicDefense: 5,
+};
+
+export const opponentStats = {
+  level: 1,
+  name: 'Vera',
+  maxHealth: 200,
+  // img: '/assets/samus.png',
+
+  magic: 13,
+  attack: 15,
+  defense: 3,
+  magicDefense: 5,
+};
