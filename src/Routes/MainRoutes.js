ed93d4e140d8d7b29c571ca025c1783409d95a7d
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2023-03-20 14:26:22 +0700

    signup

--- src/Routes/MainRoutes.js
+++ src/Routes/MainRoutes.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Routes, Route, } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Layouting } from "../PageLayout/Layouting";
-import { Home }  from "../Pages/Home/Home";
+import { Home } from "../Pages/Home/Home";
 import { Games } from "../Pages/Games/cardgame/Games";
 import { MainStory } from "../Pages/MainStory/Mainstory";
 import { Characters } from "../Pages/Character/Character";
@@ -10,7 +10,6 @@ import { Rule } from "../Pages/Rule/Rule";
 import { Forum } from "../Pages/Forum/Forum";
 // import { Home, Characters, Games, MainStory, Ability, Rule, Forum,} from "Pages";
 import { Loading } from "../animation/loading";
-
 import { Login } from "../auth/login";
 import { SignUp } from "../auth/signup";
 // import { Stopwatch } from "../Pages/Games/settimer"
@@ -18,20 +17,21 @@ import { SignUp } from "../auth/signup";
 export const MainRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<Layouting />}>
-            <Route index path="/Home" element={<Home />} />
-            <Route path="/Games" element={<Games />} />
-            <Route path="/character" element={<Characters />}/>
-            <Route path="/perks" element={<Ability />} />
-            <Route path="/rules" element={<Rule />} />
-            <Route path="/story" element={<MainStory />} />
-            <Route path="/forum" element={<Forum />} />
-        </Route>
-          <Route path="/loading" element={<Loading />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          {/* <Route path="/timer" element={<Stopwatch  />} /> */}
+      <Route>
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/timer" element={<Stopwatch  />} /> */}
+      </Route>
+      <Route path="/" element={<Layouting />}>
+        <Route index path="/Home" element={<Home />} />
+        <Route path="/Games" element={<Games />} />
+        <Route path="/character" element={<Characters />} />
+        <Route path="/perks" element={<Ability />} />
+        <Route path="/rules" element={<Rule />} />
+        <Route path="/story" element={<MainStory />} />
+        <Route path="/forum" element={<Forum />} />
+      </Route>
     </Routes>
-  )
-}
-
+  );
+};
--- src/auth/signup.js
+++ src/auth/signup.js
@@ -1,83 +1,430 @@
-import { useRef, useState, useEffect } from "react";
-import { Button, Checkbox, Form, Input } from "antd";
-import "./auth.css";
+// import { useRef, useState, useEffect } from "react";
+// import { Button, Checkbox, Form, Input } from "antd";
+// import "./auth.css";
 
-export const SignUp = () => {
-  const userRef = useRef();
-  const errRef = useRef();
+// export const SignUp = () => {
+//   const userRef = useRef();
+//   const errRef = useRef();
 
-  const [user, setUser] = useState("");
-  const [pwd, setPwd] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [success, setSuccess] = useState(false);
+//   const [user, setUser] = useState("");
+//   const [pwd, setPwd] = useState("");
+//   const [errMsg, setErrMsg] = useState("");
+//   const [success, setSuccess] = useState(false);
 
-  useEffect(() => {
-    userRef.current.focus();
-  }, []);
+//   useEffect(() => {
+//     userRef.current.focus();
+//   }, []);
 
-  useEffect(() => {
-    setErrMsg("");
-  }, [user, pwd]);
+//   useEffect(() => {
+//     setErrMsg("");
+//   }, [user, pwd]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(user, pwd);
-    setUser("");
-    setPwd("");
-    setSuccess(true);
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     console.log(user, pwd);
+//     setUser("");
+//     setPwd("");
+//     setSuccess(true);
+//   };
+
+//   return (
+//     <>
+//       {success ? (
+//         <section>
+//           <h1>You are logged in!</h1>
+//           <br />
+//           <p>
+//             <a href="/login">Go to login page</a>
+//           </p>
+//         </section>
+//       ) : (
+//         <section>
+//           <p
+//             ref={errRef}
+//             className={errMsg ? "errmsg" : "offscreen"}
+//             aria-live="assertive"
+//           >
+//             {errMsg}
+//           </p>
+//           <h1>REGISTER</h1>
+//           <Form onSubmit={handleSubmit} className="slide-2">
+//             <label htmlFor="">Username</label>
+//             <Input
+//               type="text"
+//               id="username"
+//               ref={userRef}
+//               autoComplete="off"
+//               onChange={(e) => setUser(e.target.value)}
+//               value={user}
+//               required
+//             />
+//             <label htmlFor="password">Password</label>
+//             <input
+//               type="password"
+//               id="password"
+//               onChange={(e) => setPwd(e.target.value)}
+//               value={pwd}
+//               required
+//             />
+//             <Button>SignUp</Button>
+//           </Form>
+//           <p>
+//             Already have an account?
+//             <br />
+//             <span className="line">
+//               {/*put router link here*/}
+//               <a href="/login">login</a>
+//             </span>
+//           </p>
+//         </section>
+//       )}
+//     </>
+//   );
+// };
+
+import {
+  AutoComplete,
+  Button,
+  Cascader,
+  Checkbox,
+  Col,
+  Form,
+  Input,
+  InputNumber,
+  Row,
+  Select,
+} from 'antd';
+import { useState } from 'react';
+const { Option } = Select;
+const residences = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+export const SignUp= () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
   };
-
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
+  const suffixSelector = (
+    <Form.Item name="suffix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="USD">$</Option>
+        <Option value="CNY">¥</Option>
+      </Select>
+    </Form.Item>
+  );
+  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
+  const onWebsiteChange = (value) => {
+    if (!value) {
+      setAutoCompleteResult([]);
+    } else {
+      setAutoCompleteResult(['.com', '.org', '.net'].map((domain) => `${value}${domain}`));
+    }
+  };
+  const websiteOptions = autoCompleteResult.map((website) => ({
+    label: website,
+    value: website,
+  }));
   return (
-    <>
-      {success ? (
-        <section>
-          <h1>You are logged in!</h1>
-          <br />
-          <p>
-            <a href="/login">Go to login page</a>
-          </p>
-        </section>
-      ) : (
-        <section>
-          <p
-            ref={errRef}
-            className={errMsg ? "errmsg" : "offscreen"}
-            aria-live="assertive"
-          >
-            {errMsg}
-          </p>
-          <h1>REGISTER</h1>
-          <Form onSubmit={handleSubmit} className="slide-2">
-            <label htmlFor="">Username</label>
-            <Input
-              type="text"
-              id="username"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setUser(e.target.value)}
-              value={user}
-              required
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPwd(e.target.value)}
-              value={pwd}
-              required
-            />
-            <Button>SignUp</Button>
-          </Form>
-          <p>
-            Already have an account?
-            <br />
-            <span className="line">
-              {/*put router link here*/}
-              <a href="/login">login</a>
-            </span>
-          </p>
-        </section>
-      )}
-    </>
+    <div>
+    <h1>REGISTER</h1>
+    <Form
+      className='slide-2'
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      initialValues={{
+        residence: ['zhejiang', 'hangzhou', 'xihu'],
+        prefix: '86',
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('The two passwords that you entered do not match!'));
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="nickname"
+        label="Nickname"
+        tooltip="What do you want others to call you?"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your nickname!',
+            whitespace: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="residence"
+        label="Habitual Residence"
+        rules={[
+          {
+            type: 'array',
+            required: true,
+            message: 'Please select your habitual residence!',
+          },
+        ]}
+      >
+        <Cascader options={residences} />
+      </Form.Item>
+
+      <Form.Item
+        name="phone"
+        label="Phone Number"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your phone number!',
+          },
+        ]}
+      >
+        <Input
+          addonBefore={prefixSelector}
+          style={{
+            width: '100%',
+          }}
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="donation"
+        label="Donation"
+        rules={[
+          {
+            required: true,
+            message: 'Please input donation amount!',
+          },
+        ]}
+      >
+        <InputNumber
+          addonAfter={suffixSelector}
+          style={{
+            width: '100%',
+          }}
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="website"
+        label="Website"
+        rules={[
+          {
+            required: true,
+            message: 'Please input website!',
+          },
+        ]}
+      >
+        <AutoComplete options={websiteOptions} onChange={onWebsiteChange} placeholder="website">
+          <Input />
+        </AutoComplete>
+      </Form.Item>
+
+      <Form.Item
+        name="intro"
+        label="Intro"
+        rules={[
+          {
+            required: true,
+            message: 'Please input Intro',
+          },
+        ]}
+      >
+        <Input.TextArea showCount maxLength={100} />
+      </Form.Item>
+
+      <Form.Item
+        name="gender"
+        label="Gender"
+        rules={[
+          {
+            required: true,
+            message: 'Please select gender!',
+          },
+        ]}
+      >
+        <Select placeholder="select your gender">
+          <Option value="male">Male</Option>
+          <Option value="female">Female</Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item label="Captcha" extra="We must make sure that your are a human.">
+        <Row gutter={8}>
+          <Col span={12}>
+            <Form.Item
+              name="captcha"
+              noStyle
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the captcha you got!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Button>Get captcha</Button>
+          </Col>
+        </Row>
+      </Form.Item>
+
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+          },
+        ]}
+        {...tailFormItemLayout}
+      >
+        <Checkbox>
+          I have read the <a href="">agreement</a>
+        </Checkbox>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
+    </div>
   );
 };
